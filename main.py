04b6b8e9998c5c109a5e7b9d4e69e00001885232
Artishevskiy <andrew121205@gmail.com>
2021-12-20 12:28:23 +0500

    NewCommitForVK

--- main.py
+++ main.py
@@ -2,24 +2,23 @@ import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
 
+hellos = ['Привет', 'привет', 'прив', 'ку', 'ку', 'здаров', 'здарова', 'q', 'куку', 'ку-ку', 'здравствуйте', 'здравствуй']
+
 
 def main():
     vk_session = vk_api.VkApi(
         token='64f7d38df6cbac49f2146d5037a93647b83f9897e355478551f3bee2d393cc2a8f57aefd5803bf5b88750')
-
     longpoll = VkBotLongPoll(vk_session, 203632426)
-
     for event in longpoll.listen():
-
         if event.type == VkBotEventType.MESSAGE_NEW:
-            print(event)
-            print('Новое сообщение:')
-            print('Для меня от:', event.obj.message['from_id'])
-            print('Текст:', event.obj.message['text'])
-            print(1)
             vk = vk_session.get_api()
-            vk.messages.send(user_id=event.obj.message['from_id'],
-                             message="Спасибо, что написали нам. Мы обязательно ответим",
+            ID = event.obj.message['from_id']
+            response = vk.users.get(user_ids=ID)
+            first_name = response[0]['first_name']
+            last_name = response[0]['last_name']
+            txt_msg = event.obj.message['text']
+            vk.messages.send(user_id=ID,
+                             message=first_name + ' ' + last_name,
                              random_id=random.randint(0, 2 ** 64))
 
 
